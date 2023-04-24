6d686ed3c15ec3ff41c7c5321465765f6de395e6
younwony <wony9324@naver.com>
2023-04-24 21:37:47 +0900

    init()

---
+++ src/test/java/dev/wony/effectivejava/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wony.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
