21c4c71e99e556494f28188b3b6d25c06ac89d81
younwony <wony9324@naver.com>
2023-04-25 21:42:57 +0900

    feat: Chapter 1 - Item 3 : private 생성자나 열거 타입으로 싱글턴임을 보증하라

---
+++ src/main/java/dev/wony/effectivejava/chapter_1/item_3/item_3.java
@@ -0,0 +1,20 @@
+package dev.wony.effectivejava.chapter_1.item_3;
+
+/**
+ * 아이템 3. private 생성자나 열거 타입으로 싱글턴임을 보증하라
+ */
+public class item_3 {
+    public static void main(String[] args) {
+        // public static final 필드 방식의 싱글턴
+        Elvis elvis = Elvis.INSTANCE;
+        elvis.leaveTheBuilding();
+
+        // 정적 팩터리 방식의 싱글턴
+        Elvis elvis2 = Elvis.getInstance();
+        elvis2.leaveTheBuilding2();
+
+        // 열거 타입 방식의 싱글턴
+        Elvis.Elvis3 elvis3 = Elvis.Elvis3.INSTANCE;
+        elvis3.leaveTheBuilding();
+    }
+}
