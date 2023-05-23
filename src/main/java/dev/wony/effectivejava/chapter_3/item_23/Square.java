cf307fa2e770ca4fc1021da15642c9b97076c6aa
younwony <wony9324@naver.com>
2023-05-23 20:37:59 +0900

    feat: Chapter 1 - Item 23 : 태그 달린 클래스보다는 클래스 계층구조를 활용하라

---
+++ src/main/java/dev/wony/effectivejava/chapter_3/item_23/Square.java
@@ -0,0 +1,12 @@
+package dev.wony.effectivejava.chapter_3.item_23;
+
+public class Square extends Rectangle{
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public double area() {
+        return super.area();
+    }
+}
